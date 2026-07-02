85823c0b6f20e8c7428c9b348938e4345cf04c8b
Arpan Dey <arpandeyarpan871@gmail.com>
2026-07-02 10:30:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const client = new OpenAI({
-  apiKey: "YOUR_API_KEY"
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
 });
 
 app.post("/solve", async (req, res) => {
